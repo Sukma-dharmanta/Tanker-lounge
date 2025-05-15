7641fa647cae68c27f7db081afbe83f748b88164
Sukma-dharmanta <sukmadharmantap@gmail.com>
2025-05-15 13:32:24 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.title("Tanks")
-st.image("view images.jpg", width=300)
+st.image("view https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/M1A2_SEP_v3.jpg/960px-M1A2_SEP_v3.jpg", width=300)
 st.write(
     "/n.")
 st.subheader("Tank, 120mm General Dynamics M1A2 Abrams SEP TUSK V1") 
