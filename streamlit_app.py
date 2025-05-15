0f8b08a0c7699d0b3c7d11b2166d71d08a20844f
Sukma-dharmanta <sukmadharmantap@gmail.com>
2025-05-15 13:40:07 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 st.title("Tanks") 
 st.write(
-    "/n.")
+    "Welcome.")
 st.subheader("Tank, 120mm General Dynamics M1A2 Abrams SEP TUSK V1") 
 st.write("Want some classified documents?") 
